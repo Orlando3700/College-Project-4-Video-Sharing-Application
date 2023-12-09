f6022a8e4f7894b2ab021520f8cf268ee38c1b25
Orlando3700 <143731499+Orlando3700@users.noreply.github.com>
2023-12-09 06:42:57 -0500

    Added session.js file

---
+++ Logic/session.js
@@ -0,0 +1,15 @@
+const session = require('express-session');
+const app = express();
+
+// Register the 'session' middleware. The middleware will append
+//the session to the req object of every route (i.e. req.session)
+const middlewareSession=(session({
+    secret: "secretKey",
+    saveUninitialized: true,
+    resave: false,
+    cookie: { maxAge: 6000 }}));
+
+// To make the routes available we need to export the router object.
+// now the router object can be imported by other modules in our applications.
+module.exports = middlewareSession;
+
